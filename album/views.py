2152faab30c62c8dd81707f498bb085e7c745a13
SegundoPaladines <segundopaladines@gmail.com>
2023-10-25 16:47:55 -0500

    Primer commit

---
+++ album/views.py
@@ -0,0 +1,24 @@
+from django.urls import reverse_lazy 
+from django.views.generic import CreateView, DeleteView, UpdateView, ListView, DetailView
+from album.models import Selection, Player
+
+# Create your views here.
+#todas las selecciones
+class SelectionListView(ListView):
+    model = Selection
+    #template_name = "./templates/album/selection_list.html"
+
+#solo una seleccion
+class SelectionDetailView(DetailView):
+    model = Selection
+    #template_name = "./templates/album/selection_detail.html"
+
+#todos los jugadores
+class PlayerListView(ListView):
+    model = Player
+    #template_name = "./templates/album/player_list.html"
+
+#solo un jugador
+class PlayerDetailView(DetailView):
+    model = Player
+    #template_name = "./templates/album/player_detail.html"
